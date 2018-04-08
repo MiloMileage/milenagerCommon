d3ae8cafd9bd6ba2851a10d5c45551d6f77beda6
Yair Levi <lyair1@gmail.com>
2018-04-07 18:05:27 -0700

    add stuff to YMUserData (state)

--- src/common/YMUserData.ts
+++ src/common/YMUserData.ts
@@ -2,6 +2,7 @@ import YMDrive from './YMDrive'
 import YMSavedLocation from './YMSavedLocation'
 import YMUserSettings from './YMUserSettings'
 import YMRateIRS from './YMRateIRS'
+import YMDateRange from './YMDateRange'
 
 export default class YMUserData {
     drives: Array<YMDrive>
@@ -9,15 +10,19 @@ export default class YMUserData {
     rates: Map<string, YMRateIRS>
     userSettings: YMUserSettings
     globalSettings: YMUserSettings
+    fcmToken: string
+    dateRange: YMDateRange
+    loading: boolean
+
     // selectedDrivesIds: Array<string>
     // filterSearch: string
-    // dateRange: YMDateRange
-    // isLoading: boolean
 
     constructor (drives: Array<YMDrive> = [], savedLocations: Map<string, YMSavedLocation> = new Map<string, YMSavedLocation>(),
             rates: Map<string, YMRateIRS> = new Map<string, YMRateIRS>(),
             userSettings: YMUserSettings = new YMUserSettings(),
-            globalSettings: YMUserSettings = new YMUserSettings()) {
+            globalSettings: YMUserSettings = new YMUserSettings(),
+            dateRange: YMDateRange = new YMDateRange(undefined, undefined),
+            loading: boolean = false) {
         this.drives = drives.map(x => YMDrive.fromObject(x))
 
         for (const key in savedLocations) {
@@ -32,6 +37,8 @@ export default class YMUserData {
 
         this.userSettings = YMUserSettings.fromObject(userSettings)
         this.globalSettings = YMUserSettings.fromObject(globalSettings)
+        this.dateRange = dateRange
+        this.loading = loading
     }
 
     // tslint:disable-next-line:member-ordering
@@ -39,6 +46,6 @@ export default class YMUserData {
         if(obj == null) return new YMUserData([], new Map<string, YMSavedLocation>(), new Map<string, YMRateIRS>(),
         YMUserSettings.fromObject(undefined), YMUserSettings.fromObject(undefined))
 
-        return new YMUserData(obj.drives, obj.savedLocations, obj.rates, obj.userSettings, obj.globalSettings)
+        return new YMUserData(obj.drives, obj.savedLocations, obj.rates, obj.userSettings, obj.globalSettings, obj. dateRange, obj.loading)
     }
 }
