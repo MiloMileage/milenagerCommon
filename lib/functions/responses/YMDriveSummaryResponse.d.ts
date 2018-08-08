cdc09dfdee0489b278481e03c1f24c8b2f1959fe
Yair Levi <lyair1@gmail.com>
2018-08-08 16:34:34 -0700

    add logged miles and total miles to drive summary

--- lib/functions/responses/YMDriveSummaryResponse.d.ts
+++ lib/functions/responses/YMDriveSummaryResponse.d.ts
@@ -5,12 +5,13 @@ export default class YMDriveSummaryResponse {
     };
     earned: number;
     potential: number;
-    miles: number;
+    loggedMiles: number;
+    totalMiles: number;
     dateRange: YMDateRange;
     parkingMoney: number;
     tollMoney: number;
     constructor(drivesCount: {
         [purposeId: string]: number;
-    }, earned: number, potential: number, miles: number, dateRange: YMDateRange, parkingMoney: number, tollsMoney: number);
+    }, earned: number, potential: number, loggedMiles: number, totalMiles: number, dateRange: YMDateRange, parkingMoney: number, tollsMoney: number);
     static fromObject: (obj: any) => YMDriveSummaryResponse;
 }
--- src/functions/responses/YMDriveSummaryResponse.ts
+++ src/functions/responses/YMDriveSummaryResponse.ts
@@ -4,7 +4,8 @@ export default class YMDriveSummaryResponse {
     drivesCount: { [purposeId: string]: number }
     earned: number
     potential: number
-    miles: number
+    loggedMiles: number
+    totalMiles: number
     dateRange: YMDateRange
     parkingMoney: number
     tollMoney: number
@@ -13,14 +14,16 @@ export default class YMDriveSummaryResponse {
         drivesCount: { [purposeId: string]: number },
         earned: number,
         potential: number,
-        miles: number,
+        loggedMiles: number,
+        totalMiles: number,
         dateRange: YMDateRange,
         parkingMoney: number,
         tollsMoney: number) {
         this.drivesCount = drivesCount
         this.earned = earned
         this.potential = potential
-        this.miles = miles
+        this.loggedMiles = loggedMiles
+        this.totalMiles = totalMiles
         this.dateRange = dateRange
         this.parkingMoney = parkingMoney
         this.tollMoney = tollsMoney
@@ -28,8 +31,8 @@ export default class YMDriveSummaryResponse {
 
     // tslint:disable-next-line:member-ordering
     static fromObject = function(obj: any) {
-        if(obj == null) return new YMDriveSummaryResponse({}, 0, 0, 0, YMDateRange.fromObject({}), 0, 0)
+        if(obj == null) return new YMDriveSummaryResponse({}, 0, 0, 0, 0, YMDateRange.fromObject({}), 0, 0)
 
-        return new YMDriveSummaryResponse(obj.drivesCount, obj.earned, obj.potential, obj.miles, obj.dateRange, obj.parkingMoney, obj.tollMoney)
+        return new YMDriveSummaryResponse(obj.drivesCount, obj.earned, obj.potential, obj.loggedMiles, obj.totalMiles, obj.dateRange, obj.parkingMoney, obj.tollMoney)
     }
 }
