6cae193139f8c29b4932509c5eb7226d24f2ab8d
Yair Levi <lyair1@gmail.com>
2018-08-18 21:57:18 -0700

    fix null bug in YMDateRange

--- src/common/YMDateRange.ts
+++ src/common/YMDateRange.ts
@@ -3,8 +3,8 @@ export default class YMDateRange {
     endDate: Date
 
     constructor (startDate: Date, endDate: Date) {
-        this.startDate = new Date(startDate)
-        this.endDate = new Date(endDate)
+        this.startDate = startDate
+        this.endDate = endDate
     }
 
     isEqualTo(anotherDateRange: YMDateRange) {
