cb9a3ef942ad179d1b0801df65642fe8831c7d7d
Yair Levi <lyair1@gmail.com>
2019-01-24 16:43:54 -0800

    add tutorial view to global settings

--- src/common/YMGlobalUserSettings.ts
+++ src/common/YMGlobalUserSettings.ts
@@ -5,27 +5,31 @@ import YMPersonalSettings from './YMPersonalSettings'
 import YMAutoLocationRule from './YMAutoLocationRule'
 import YMRate from './YMRate'
 import YMWorkingHour from './YMWorkingHour'
+import YMTutorialView from './YMTutorialView'
 
 export default class YMGlobalUserSettings {
     purposes: Array<YMPurpose>
     notificationSettings: Array<YMNotificationSetting>
     workingHours: Array<YMWorkingHour>
     irsRates: Map<string, Map<string, number>>
+    tutorialViews: Array<YMTutorialView>
 
     constructor (purposes: Array<YMPurpose> = [],
         notificationSettings: Array<YMNotificationSetting> = [],
         workingHours: Array<YMWorkingHour> = [],
-        irsRates: Map<string, Map<string, number>>) {
+        irsRates: Map<string, Map<string, number>>,
+        tutorialViews: Array<YMTutorialView> = []) {
         this.purposes = purposes.map(x => YMPurpose.fromObject(x))
         this.notificationSettings = notificationSettings.map(x => YMNotificationSetting.fromObject(x))
         this.workingHours = workingHours.map(x => YMWorkingHour.fromObject(x))
         this.irsRates = irsRates
+        this.tutorialViews = tutorialViews
     }
 
     // tslint:disable-next-line:member-ordering
     static fromObject(obj) {
-        if(obj == null) return new YMGlobalUserSettings([], [], [], new Map<string, Map<string, number>>())
+        if(obj == null) return new YMGlobalUserSettings([], [], [], new Map<string, Map<string, number>>(), [])
 
-        return new YMGlobalUserSettings(obj.purposes, obj.notificationSettings, obj.workingHours, obj.irsRates)
+        return new YMGlobalUserSettings(obj.purposes, obj.notificationSettings, obj.workingHours, obj.irsRates, obj.tutorialViews)
     }
 }
