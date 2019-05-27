4fecb58cf872448cd78045e184ec111dc11d5014
Yair Levi <lyair1@gmail.com>
2019-05-26 18:54:02 -0700

    update user history to not have patently if has subscription

--- src/functions/responses/YMUserUsageHistory.test.ts
+++ src/functions/responses/YMUserUsageHistory.test.ts
@@ -19,10 +19,28 @@ test('Subscription ended less than a month', () => {
     expect(history.monthsToPayFor()).toBe(0)
 });
 
+test('no subscription ended less than a month', () => {
+    const history = new YMUserUsageHistory(false, new Date(), Moment().subtract(6, 'day').toDate())
+
+    expect(history.monthsToPayFor()).toBe(0)
+});
+
+test('no subscription ended a month and few days ago', () => {
+    const history = new YMUserUsageHistory(false, Moment().subtract(1, 'month').subtract(6, 'day').toDate(), new Date())
+
+    expect(history.monthsToPayFor()).toBe(1)
+});
+
+test('no subscription ended 2 months ago', () => {
+    const history = new YMUserUsageHistory(false, Moment().subtract(2, 'month').toDate(), new Date())
+
+    expect(history.monthsToPayFor()).toBe(2)
+});
+
 test('Subscription ended 2 months ago', () => {
     const history = new YMUserUsageHistory(true, new Date(), Moment().subtract(2, 'month').toDate())
 
-    expect(history.monthsToPayFor()).toBe(2)
+    expect(history.monthsToPayFor()).toBe(0)
 });
 
 test('create default with null', () => {
@@ -39,11 +57,11 @@ test('create default with undefined', () => {
 
 test('create default with object', () => {
     const obj = {
-        didHaveSubscription: true,
+        didHaveSubscription: false,
         firstDriveDate: Moment().subtract(10, 'month'),
         subscriptionEndTime: Moment().subtract(5, 'month'),
     }
     const history = YMUserUsageHistory.fromObject(obj)
 
-    expect(history.monthsToPayFor()).toBe(5)
+    expect(history.monthsToPayFor()).toBe(10)
 });
