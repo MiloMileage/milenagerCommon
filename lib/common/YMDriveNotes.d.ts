62949a2db85f72ee2048313ef28a9811677603ee
Yair Levi <lyair1@gmail.com>
2020-05-01 09:08:52 -0400

    add gas and others to drives notes

--- lib/common/YMDriveNotes.d.ts
+++ lib/common/YMDriveNotes.d.ts
@@ -2,6 +2,9 @@ export default class YMDriveNotes {
     note: string;
     parkingMoney: number;
     tollMoney: number;
-    constructor(note: string, parkingMoney: number, tollMoney: number);
+    gasMoney: number;
+    otherMoney: number;
+    constructor(note: string, parkingMoney: number, tollMoney: number, gasMoney: number, otherMoney: number);
+    getTotalMoney(): number;
     static fromObject: (obj: any) => YMDriveNotes;
 }
