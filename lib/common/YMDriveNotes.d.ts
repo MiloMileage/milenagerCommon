92574de5e10ac3778a12b3074f4dc7d8149bbd1d
Yair Levi <lyair1@gmail.com>
2020-05-11 19:49:33 -0400

    drop gas and others from drive notes

--- lib/common/YMDriveNotes.d.ts
+++ lib/common/YMDriveNotes.d.ts
@@ -2,9 +2,7 @@ export default class YMDriveNotes {
     note: string;
     parkingMoney: number;
     tollMoney: number;
-    gasMoney: number;
-    otherMoney: number;
-    constructor(note: string, parkingMoney: number, tollMoney: number, gasMoney: number, otherMoney: number);
+    constructor(note: string, parkingMoney: number, tollMoney: number);
     getTotalMoney(): number;
     static fromObject: (obj: any) => YMDriveNotes;
 }
--- src/common/YMDriveNotes.ts
+++ src/common/YMDriveNotes.ts
@@ -2,25 +2,21 @@ export default class YMDriveNotes {
     note: string
     parkingMoney: number
     tollMoney: number
-    gasMoney: number
-    otherMoney: number
 
-    constructor (note: string, parkingMoney: number, tollMoney: number, gasMoney: number, otherMoney: number) {
+    constructor (note: string, parkingMoney: number, tollMoney: number) {
         this.note = note
         this.parkingMoney = parkingMoney ? Number(parkingMoney) : 0
         this.tollMoney = tollMoney ? Number(tollMoney) : 0
-        this.gasMoney = gasMoney ? Number(gasMoney) : 0
-        this.otherMoney = otherMoney ? Number(otherMoney) : 0
     }
 
     getTotalMoney() {
-        return this.parkingMoney + this.tollMoney + this.gasMoney + this.otherMoney
+        return this.parkingMoney + this.tollMoney
     }
 
     // tslint:disable-next-line:member-ordering
     static fromObject = function(obj: any) {
-        if(obj == null) return new YMDriveNotes('', 0, 0, 0, 0)
+        if(obj == null) return new YMDriveNotes('', 0, 0)
 
-        return new YMDriveNotes(obj.note, obj.parkingMoney, obj.tollMoney, obj.gasMoney, obj.otherMoney)
+        return new YMDriveNotes(obj.note, obj.parkingMoney, obj.tollMoney)
     }
 }
