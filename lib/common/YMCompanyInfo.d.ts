723fcb2c5711799369dbf251580148b9f372849e
Yair Levi <lyair1@gmail.com>
2020-11-25 12:25:10 -0500

    add created data and updated date

--- lib/common/YMCompanyInfo.d.ts
+++ lib/common/YMCompanyInfo.d.ts
@@ -2,6 +2,8 @@ export default class YMCompanyInfo {
     name: string;
     creatorEmail: string;
     supportEmail: string;
-    constructor(name: string, creatorEmail: string, supportEmail: string);
+    createdDate: Date;
+    updatedDate: Date;
+    constructor(name: string, creatorEmail: string, supportEmail: string, createdDate: Date, updatedDate: Date);
     static fromObject: (obj: any) => YMCompanyInfo;
 }
--- src/common/YMCompanyInfo.ts
+++ src/common/YMCompanyInfo.ts
@@ -2,20 +2,26 @@ export default class YMCompanyInfo {
     name: string
     creatorEmail: string
     supportEmail: string
+    createdDate: Date
+    updatedDate: Date
 
-    constructor (name: string, creatorEmail: string, supportEmail: string) {
+    constructor (name: string, creatorEmail: string, supportEmail: string, createdDate: Date, updatedDate: Date) {
         this.name = name
         this.supportEmail = supportEmail
         this.creatorEmail = creatorEmail
+        this.createdDate = createdDate
+        this.updatedDate = updatedDate
     }
 
     static fromObject = function(obj: any) {
-        if(obj == null) return new YMCompanyInfo('', '', '')
+        if(obj == null) return new YMCompanyInfo('', '', '', new Date(), new Date())
         
         return new YMCompanyInfo(
             obj.name,
             obj.creatorEmail,
             obj.supportEmail,
+            obj.createdDate,
+            obj.updatedDate
         )
     }
 }
